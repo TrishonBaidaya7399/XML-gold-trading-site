b699ebe05095f439d24b4fcbe31cf0574b855acd
TrishonBaidaya7399 <githubtrishon@gmail.com>
2023-09-17 01:20:12 +0600

    first commit

---
+++ sources/js/index.js
@@ -0,0 +1,26 @@
+// function toggleActive(button) {
+//     // const buttons = document.querySelectorAll(".category");
+
+//     // buttons.forEach((btn) => {
+//     //     btn.classList.remove("text-orange-400");
+//     //     btn.querySelector(".fa-caret-down").classList.add("text-white");
+//     //     btn.querySelector("p").classList.remove("text-orange-400");
+//     //     btn.querySelector("p").classList.add("text-gray-500");
+//     // });
+
+//     // button.classList.add("text-orange-400");
+//     // button.querySelector(".fa-caret-down").classList.remove("text-white");
+//     // button.querySelector("p").classList.remove("text-gray-500");
+// }
+
+
+// function toggleActive(button) {
+//     // Remove the 'active' class from all buttons
+//     const buttons = document.querySelectorAll(".category");
+//     buttons.forEach((btn) => {
+//         btn.classList.remove("active");
+//     });
+
+//     // Add the 'active' class to the clicked button
+//     button.classList.add("active");
+// }
